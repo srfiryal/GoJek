33bd35b4eeb9cadb3f7baaf20b543ca4b45f0c41
Sarah <sarah5firyal@gmail.com>
2021-02-25 07:22:48 +0700

    adding splash screen

---
+++ app/src/main/java/com/example/linearlayout/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.linearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        /****** Create Thread that will sleep for 3 seconds****/
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    // Thread will sleep for 3 seconds
+                    sleep(3 * 1000);
+                    Intent i = new Intent(getBaseContext(), MainActivity.class);
+                    startActivity(i);
+                    finish();
+                } catch (Exception e) {
+                }
+            }
+        };
+        // start thread
+        background.start();
+    }
+}
