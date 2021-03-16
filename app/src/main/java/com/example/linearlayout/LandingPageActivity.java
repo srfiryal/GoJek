acd916e1a6136b71352d15f19691c2bba93a4a21
srfiryal <sarah5firyal@gmail.com>
2021-03-16 11:37:01 +0700

    update LandingPage

--- app/src/main/java/com/example/linearlayout/LandingPageActivity.java
+++ app/src/main/java/com/example/linearlayout/LandingPageActivity.java
@@ -14,7 +14,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -37,7 +36,7 @@ public class LandingPageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_landingpage);
 
         btn_login = findViewById(R.id.btn_login_main);
         btn_register = findViewById(R.id.btn_register_main);
